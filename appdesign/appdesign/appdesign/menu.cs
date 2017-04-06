c0d5a8c36344531ec87657ec75908ed964aecc3e
Ziggy <0920542@hr.nl>
2017-04-06 15:29:36 +0100

    menu design v1

---
+++ appdesign/appdesign/appdesign/menu.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace appdesign
+{
+    public partial class Parkingscouting : Form
+    {
+        public Parkingscouting()
+        {
+            InitializeComponent();
+        }
+
+        private void parkingbtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chargingbtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void eventbtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void instructionsbtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void creditsbtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exitbtn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
