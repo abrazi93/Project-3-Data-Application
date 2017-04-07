4b69ae7773aee23b24151c1ee08c6fd1ee965ff7
Ziggy <0920542@hr.nl>
2017-04-07 13:38:27 +0100

    SQL test (zoeken met deel v. woord)
    ook met zoeken met delen van een woord gebruiken we LIKE '%<inputhier>%'

--- tests/voorbeeld.sql
+++ tests/voorbeeld.sql
@@ -11,4 +11,18 @@ insert into "test" ("id", "districts") values
 (5, 'Schiedam'),
 (6, 'Schiedam');
 
+-- Test nr 1:
+
 SELECT DISTINCT "districts" FROM "test";
+
+-- Test nr 2:
+
+SELECT DISTINCT "districts" FROM "test" WHERE "districts" LIKE '%dam%'
+
+-- of zonder DISTINCT
+
+SELECT "districts" FROM "test" WHERE "districts" LIKE '%dam%'
+
+-- of met meerdere velden zonder DISTINCT
+
+SELECT * FROM "test" WHERE "districts" LIKE '%dam%'
